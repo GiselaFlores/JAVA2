5a85499cd8ed69fc6bb6c088e6b520cb1ea712f1
GiselaFlores <gi.flores@bue.edu.ar>
2024-04-24 17:03:15 -0300

    js intro

---
+++ index.js
@@ -0,0 +1,32 @@
+/* comentario */
+// comentario
+
+/* variables */
+// tipo + nombre = valor;
+let nombre= "Roberto";
+let edad=45;
+let edad2="40";
+let saludo= "Hola";
+let numeroDos= 10.5;
+
+console.log(nombre);
+console.log(edad);
+console.log(edad2);
+console.log(edad2*nombre);
+
+let resultado= edad*edad2;
+console.log(resultado);
+
+resultado=5;
+console.log(resultado*10);
+
+console.log(saludo + " " + nombre + " bienvenido" + " ,tu edad es: " + edad);
+
+/* constantes */
+
+const PI= 3.14;
+
+let radio= PI*50;
+console.log(radio);
+
+let booleanos=true;
