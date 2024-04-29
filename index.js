d2fbf2235f97d02cdf4cebc8a28cbfbcd55af695
GiselaFlores <gi.flores@bue.edu.ar>
2024-04-29 16:16:58 -0300

    estructuras de control

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
-/* comentario */
-// comentario
+/* comentario 
+// comentario de linea
 
-/* variables */
-// tipo + nombre = valor;
-let nombre= "Roberto";
-let edad=45;
-let edad2="40";
-let saludo= "Hola";
-let numeroDos= 10.5;
+// declaracion + nombre + = + contenido;
 
+let nombre="Roberto";
+let numero=45;
+let numeroDos="40";
+let edad;
+
+let nombreDos, num=10, sapo="pepe";
+
+edad= num*numero;
+
+console.log(edad*20);
+
+console.log("Bienvenido " + nombre + " sos el alumno numero: " + num);
+
+constantes 
+
+const PI=3.14;
+
+numeroDos= PI*10;
+console.log(numeroDos);
+
+console.log(PI);
+
+let booleana=true;
+
+let objeto={};
+
+
+
+let nombre =  prompt("Ingrese su nombre:");
 console.log(nombre);
-console.log(edad);
-console.log(edad2);
-console.log(edad2*nombre);
+*/
+//document.write("Hola bienvenido " + nombre);
 
-let resultado= edad*edad2;
-console.log(resultado);
+//let edadDos, edadTres=50, edadCuatro="40";
 
-resultado=5;
-console.log(resultado*10);
+let edad =  parseInt(prompt("Ingrese su edad:"));
+let profe =  prompt("Ingrese su carrera:");
+/*
+edadDos = edad + 10;
+edad += 10;
+edad = edad +10;
 
-console.log(saludo + " " + nombre + " bienvenido" + " ,tu edad es: " + edad);
+console.log("Tu edad en 10 años será de: " + edad);
 
-/* constantes */
+document.write("Tu edad en 10 años será de: " + edad);
+*/
 
-const PI= 3.14;
+if (edad >= 18) {
+        document.write("Tenes permiso para navegar en el sitio");
+} else {
+    document.write("NO TENES permiso para navegar en el sitio");
+}
 
-let radio= PI*50;
-console.log(radio);
+let nota = parseInt(prompt("Ingrese su nota:"));
 
-let booleanos=true;
+let calificacion = nota>=7 ? "aprobado" : "desaprobado";
+console.log(calificacion);
+
+if (edad >= 18) {
+    document.write("verifique los padrones");
+} else if (edad === 16 || edad === 17){
+    document.write("podes votar");
+} else {
+    document.write("no podes podes votar");
+}
